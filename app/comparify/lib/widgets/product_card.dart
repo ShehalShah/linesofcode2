e72e0e76345941dd4ec8b77f583e642d26e5c686
Het Nakhua <nakhuahet@gmail.com>
2024-03-10 00:33:33 +0530

    card final done

---
+++ app/comparify/lib/widgets/product_card.dart
@@ -0,0 +1,152 @@
+import 'package:comparify/models/product_item.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import '../constants.dart';
+
+class ProductCard extends StatefulWidget {
+  final ProductItem item;
+  const ProductCard({Key? key, required this.item});
+
+  @override
+  State<ProductCard> createState() => _ProductCardState();
+}
+
+class _ProductCardState extends State<ProductCard> {
+  bool isChecked = false;
+  bool isFav = false;
+
+  Future<void> _launchUrl(String url) async {
+    if (!await launchUrl(Uri.parse(url))) {
+      throw Exception('Could not launch $url');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+          decoration: BoxDecoration(
+            color: Colors.white70,
+            borderRadius: BorderRadius.circular(16),
+            border: Border.all(color: Colors.black54, width: 1.0),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.1),
+                blurRadius: 300,
+                spreadRadius: 2,
+                offset: Offset(8, 4),
+              ),
+            ],
+          ),
+          child: ListTile(
+            onTap: () {
+              // print("itemmmmmm");
+              _launchUrl(widget.item.url);
+            },
+            contentPadding: const EdgeInsets.all(16),
+            leading: ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: Image.network(
+                widget.item.image,
+                // scale: 1.2,
+                width: 80,
+                height: 200,
+                fit: BoxFit.cover,
+              ),
+            ),
+            title: Text(
+              widget.item.title,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+              overflow: TextOverflow.ellipsis,
+              maxLines: 3,
+            ),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 4),
+                Row(
+                  children: [
+                    Text(
+                      widget.item.price,
+                      style: TextStyle(
+                        color: Colors.grey[700],
+                        fontSize: 16,
+                      ),
+                    ),
+                    // favourites icon
+                    const Spacer(),
+                    IconButton(
+                      onPressed: () {
+                        setState(() {
+                          isFav = !isFav;
+                        });
+                      },
+                      icon: Icon(
+                        isFav ? Icons.favorite : Icons.favorite_border,
+                        color: isFav ? Colors.red[700] : Colors.red[700],
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 4),
+                Row(
+                  children: [
+                    const Icon(Icons.star, color: Colors.amber, size: 16),
+                    const SizedBox(width: 5),
+                    Text(
+                      widget.item.rating,
+                      style: const TextStyle(
+                        color: Colors.black54,
+                        fontSize: 12,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 4),
+                Row(
+                  children: [
+                    Spacer(),
+
+                    // compare checkbox
+                    Row(
+                      children: [
+                        Text("Compare"),
+                        Checkbox(
+                          value: isChecked,
+                          onChanged: (value) {
+                            setState(() {
+                              isChecked = value!;
+                            });
+                          },
+                          activeColor: Pallete.primaryPurple,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        Positioned(
+          top: 0,
+          left: 0,
+          child: SizedBox(
+            width: 48, // Adjust the width as needed
+            height: 48, // Adjust the height as needed
+            child: Image.asset(
+              "assets/${widget.item.from}.png",
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+      ],
+    );
+    ;
+  }
+}
