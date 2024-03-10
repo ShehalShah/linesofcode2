38d87f7795a6fbd197d0f5e8aa1f119b44e6bbf0
AdityaSurve <surveaditya521@gmail.com>
2024-03-10 11:04:15 +0530

    loader

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,22 +6,40 @@ import ProductPage from "./pages/ProductPage";
 import ComparePage from "./pages/ComparePage";
 import ImageAnalyzer from "./pages/ImageAnalyzer";
 import Chat from './pages/Chat';
+import Loader from "./components/Loader";
 
 const App = () => {
+  const [isLoading, setIsLoading] = React.useState(true);
   return (
     <div className="w-screen h-screen overflow-hidden flex items-center justify-center">
       <Router>
         <Routes>
-          <Route path="/image" element={<ImageAnalyzer />} />
-          <Route path="/compare" element={<ComparePage />} />
-          <Route path="/product" element={<ProductPage />} />
-          <Route path="/auth" element={<AuthModule />} />
-          <Route path="/" element={<MainModule />} />
+          <Route
+            path="/image"
+            element={<ImageAnalyzer setIsLoading={setIsLoading} />}
+          />
+          <Route
+            path="/compare"
+            element={<ComparePage setIsLoading={setIsLoading} />}
+          />
+          <Route
+            path="/product"
+            element={<ProductPage setIsLoading={setIsLoading} />}
+          />
+          <Route
+            path="/auth"
+            element={<AuthModule setIsLoading={setIsLoading} />}
+          />
+          <Route
+            path="/"
+            element={<MainModule setIsLoading={setIsLoading} />}
+          />
         </Routes>
       </Router>
       <Chat />
+      {isLoading && <Loader />}
     </div>
   );
-}
+};
 
 export default App
