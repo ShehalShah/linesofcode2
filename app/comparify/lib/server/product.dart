63ea5d9f0127ab9f0f1dcd23edb29f221ca4ed1d
udayanvats <udayan.asus06@gmail.com>
2024-03-10 06:34:32 +0530

    Search data and filtering done

--- app/comparify/lib/server/product.dart
+++ app/comparify/lib/server/product.dart
@@ -4,12 +4,16 @@ import 'package:http/http.dart' as http;
 
 class Product {
   Future<void> getProduct(String query) async {
-    Uri uri = Uri.parse('http://10.120.133.103:5001/api/products/search');
-    final res =
-        await http.post(uri, body: jsonEncode({"query": query.toString()}));
+    Uri uri = Uri.parse('http://10.120.133.92:5001/api/products/search');
+    final res = await http.post(uri,
+        body: jsonEncode(
+          {
+            "query": query.toString(),
+          },
+        ),
+        headers: {'Content-Type': 'application/json'});
     print("res success");
 
-    // headers: {'Content-Type': 'application/json'});
     final body = res.body;
     final decoded = jsonDecode(body);
     print(decoded);
